d86d82bda8b75921213c244bf45a14579d28df38
Florian <erri120@users.noreply.github.com>
2019-03-30 17:11:12 +0100

    Reverted changes to Board.gb
    Rolled back some changes and redid the code for the board.

--- scripts/Board.gd
+++ scripts/Board.gd
@@ -1,97 +1,66 @@
 extends TileMap
 
-export (int) var board_width = 10
-export (int) var board_height = 10
+#const containing the board width and height
+var board_width = 10
+var board_height = 10
+#bool true if game is over and player lost
+var game_over = false
+#2D matrix that represents the board, contains 1(mine) or 0(empty)
+var board=[]
 
-var matrix=[]
-var lost = false
+#function showing the game over screen
+func game_over():
+	game_over = true
+	for i in get_tree().get_nodes_in_group("Control"):
+		i.show()
 
 func _ready():
-	lost = false
+	game_over = false
 	board_width = global.width
 	board_height = global.height
-	for i in range(-(board_width/2),board_width/2):
-		matrix.append([])
-		matrix[i+(board_width/2)] = []
+	#filling the board-matrix
+	for i in range(-(board_width/2), board_width/2):
+		board.append([])
+		board[i+(board_width/2)] = []
 		for j in range(-(board_height/2), board_height/2):
+			#placing cells based on width&height of the board
 			set_cell(i,j,0,false,false)
+			#getting either 1 or 0 and putting them into the board-matrix
 			randomize()
 			var rand = randi()%2+0
-			matrix[i+(board_width/2)].append([])
-			matrix[i+(board_width/2)][j+(board_height/2)] = rand
-
-func tileIndex(bombs_near):
-	var r = 3
-	if(bombs_near == 0):
-		r = 3
-	if(bombs_near == 1):
-		r = 4
-	if(bombs_near == 2):
-		r = 5
-	if(bombs_near == 3):
-		r = 6
-	if(bombs_near == 4):
-		r = 7
-	if(bombs_near == 5):
-		r = 8
-	if(bombs_near == 6):
-		r = 9
-	if(bombs_near == 7):
-		r = 10
-	if(bombs_near == 8):
-		r = 11
-	return r
-
-func bombs_near(pos):
-	var bombs = 0
-	for x in range(pos.x-1,pos.x+1):
-		if(x>=0):
-			for y in range(pos.y-1,pos.y+1):
-				if(y>= 0):
-					if(matrix[x][y]==1):
-						bombs+=1
-						print(str(x)+","+str(y)+": Bomb")
-	return bombs
-
-func change_tile(cell_pos):
-	var m_pos = cell_pos
-	m_pos.x = cell_pos.x+(board_width/2)
-	m_pos.y = cell_pos.y+(board_height/2)
-	var bombs = bombs_near(m_pos)
-	set_cellv(cell_pos,tileIndex(bombs),false,false)
-	if(bombs == 0):
-		var nomore = false
-		for x in range(cell_pos.x-1,cell_pos.x+1):
-			for y in range(cell_pos.y-1,cell_pos.y+1):
-				var xycell = Vector2(x,y)
-				if(xycell != cell_pos):
-					pass
-					#change_tile(xycell)
-
+			board[i+(board_width/2)].append([])
+			board[i+(board_width/2)][j+(board_height/2)] = rand
+			
 func _input(event):
-	if event is InputEventMouseButton && !lost:
+	#checking if player is clicking on a tile and the game is not over
+	if event is InputEventMouseButton && !game_over:
+		#getting the global position of the mouse click
 		var event_pos = event.global_position
+		#converting the global position to a map position for the TileMap
 		var cell_pos = world_to_map(event_pos)
+		#adjusting the cell position because it's always off,y is strange needs investigation
 		cell_pos.x = cell_pos.x-16
 		cell_pos.y = cell_pos.y-9.5
+		#each cell has a corresponding value inside the matrix but you cant have a negative index
 		var m_pos = cell_pos
 		m_pos.x = cell_pos.x+(board_width/2)
 		m_pos.y = cell_pos.y+(board_height/2)
-		if(m_pos.x<board_width && m_pos.y<board_height \
-		&& cell_pos.x+board_width>board_width/2 && cell_pos.y+board_height>board_height/2):
-			if (event.is_action_pressed("p_click")):
-				if(matrix[m_pos.x][m_pos.y] == 0):
-					#print("Pos: "+str(m_pos))
-					if(m_pos.x == 0):
-						pass
-					if(m_pos.y == 0):
-						pass
-					change_tile(cell_pos)
+		#checking if the click is inside the board
+		if(m_pos.x < board_width \
+		  && m_pos.y < board_height \
+		  && cell_pos.x+board_width > board_width/2 \
+		  && cell_pos.y+board_height > board_height/2):
+			#checking if player left or right clicks
+			if(event.is_action_pressed("p_click")):
+				if(board[m_pos.x][m_pos.y] == 0):
+					#player hit an empty tile
+					#TODO: change the tile to the number of mines nearby
+					set_cellv(cell_pos,4,false,false)
 				else:
-					#GAME LOST
+					#player hit a mine
 					set_cellv(cell_pos,1,false,false)
-					lost = true
-					for i in get_tree().get_nodes_in_group("Control"):
-						i.show()
-			if (event.is_action_pressed("p_flag") && global.allow_flags):
+					game_over()
+			#player can only place flags if the game mode allows it
+			if(event.is_action("p_flag") \
+			  && global.allow_flags):
 				set_cellv(cell_pos,2,false,false)
